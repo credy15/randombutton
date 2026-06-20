d098a4b88f720aa853a7fe0fb5962c10624dbd7d
credy15 <creddy16@outlook.com>
2026-06-20 10:50:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Find the button element by its ID
+const button = document.getElementById('action-btn');
+
+// Add a click function to the button
+button.addEventListener('click', () => {
+    alert('You clicked the button!');
+});
